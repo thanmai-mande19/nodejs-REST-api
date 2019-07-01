9a5b473e512dadbf969be55fff5a544a9cc6de82
Venkata Thanmai Mande <vm6710@rit.edu>
2019-07-01 15:01:27 -0400

    routes skeleton classes - requires

---
+++ routes/Product.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+var product = require("../Model/Products");
+
+
+module.exports = router;
